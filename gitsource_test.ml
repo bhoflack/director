7329ba666c26700db02176eaee302ba21c7a23a5
Brecht Hoflack <brh@melexis.com>
2015-01-08 16:08:29 +0100

    Fix gitsource_test

--- gitsource_test.ml
+++ gitsource_test.ml
@@ -2,9 +2,9 @@ open Core.Std
 open OUnit2
 
 let generate_dirname _ = 
-    assert_equal "platform-b17b4f"
+    assert_equal "cache/platform-b17b4f"
                  (Gitsource.generate_dirname "git@github.com:bhoflack/platform");
-    assert_equal "platform-32bdb5"
+    assert_equal "cache/platform-32bdb5"
                  (Gitsource.generate_dirname "git@github.com:bhoflack/platform.git")
 
 let suite =
